dcfa448de9f45ce418638a218c3f570a2938e979
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2026-04-25 06:09:01 +0000

    Angular (Investment Calculator) - Migrating to Angular Module Based Components instead of Standalone Components

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -29,19 +29,19 @@ export class InvestmentService {
         let investmentValue = initialInvestment;
 
         for (let i = 0; i < duration; i++) {
-        const year = i + 1;
-        const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-        investmentValue += interestEarnedInYear + annualInvestment;
-        const totalInterest =
-            investmentValue - annualInvestment * year - initialInvestment;
-        annualData.push({
-            year: year,
-            interest: interestEarnedInYear,
-            valueEndOfYear: investmentValue,
-            annualInvestment: annualInvestment,
-            totalInterest: totalInterest,
-            totalAmountInvested: initialInvestment + annualInvestment * year,
-        });
+            const year = i + 1;
+            const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+            investmentValue += interestEarnedInYear + annualInvestment;
+            const totalInterest =
+                investmentValue - annualInvestment * year - initialInvestment;
+            annualData.push({
+                year: year,
+                interest: interestEarnedInYear,
+                valueEndOfYear: investmentValue,
+                annualInvestment: annualInvestment,
+                totalInterest: totalInterest,
+                totalAmountInvested: initialInvestment + annualInvestment * year,
+            });
         }
 
         // this.resultsData = annualData;
